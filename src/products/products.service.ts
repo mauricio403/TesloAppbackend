19aae0f9e65961d9fb9d6e0b65b1f9ee7259a257
Luis Mauricio Matango Mendoza <lmatango@ec.krugercorp.com>
2023-06-19 15:07:21 -0500

    add create product

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, Logger, BadRequestException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+
+
+  private readonly logger = new Logger('ProductsService');
+
+
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+
+  ) { }
+
+
+  async create(createProductDto: CreateProductDto) {
+
+    try {
+      //creacion de registro o instancia
+      const product = this.productRepository.create(createProductDto);
+
+      //impacto en BD
+      await this.productRepository.save(product);
+      return product;
+
+
+    } catch (error) {
+      this.handleDBExpeptions(error)
+    }
+
+
   }
 
   findAll() {
@@ -22,5 +51,13 @@ export class ProductsService {
 
   remove(id: number) {
     return `This action removes a #${id} product`;
+  }
+
+  private handleDBExpeptions(error: any) {
+    if (error.code == '23505')
+      throw new BadRequestException(error.detail)
+
+    this.logger.error(error)
+    throw new InternalServerErrorException('Unexecpted error, check server logs')
   }
 }
