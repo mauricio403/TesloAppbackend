b76ab95e05e185ac46a053632251b5458576e371
Luis Mauricio Matango Mendoza <lmatango@ec.krugercorp.com>
2023-07-26 16:54:28 -0500

    add upload image validations

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,32 +1,47 @@
-import { Controller, Post, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors, BadRequestException, Get, Param, Res } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter } from './helpers/fileFilter';
 import { diskStorage } from 'multer';
+import { fileNamer } from './helpers/fileNamer';
+import { Response } from 'express';
 
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) { }
 
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string) {
+
+    const path = this.filesService.getStaticProductImage(imageName);
+
+    res.sendFile(path);
+  }
+
+
 
   @Post('product')
-  @UseInterceptors( FileInterceptor('file', {
+  @UseInterceptors(FileInterceptor('file', {
     fileFilter: fileFilter,
     // limits: {fileSize:1000}
-    storage:diskStorage({
-      destination:'./static/uploads'
+    storage: diskStorage({
+      destination: './static/products',
+      filename: fileNamer
     })
   }))
   uploadProductImage(
-    @UploadedFile()  file: Express.Multer.File,
+    @UploadedFile() file: Express.Multer.File,
   ) {
-    if(!file){
+    if (!file) {
       throw new BadRequestException('Make sure that file is an image')
     }
+    const secureUrl = `${file.filename}`;
 
     return {
-      fileName: file.originalname
+      fileName: secureUrl
     }
   }
 
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,7 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { existsSync } from 'fs';
+import { join } from 'path';
 
 
 @Injectable()
 export class FilesService {
+
+
+    getStaticProductImage(imageName: string) {
+        const path = join(__dirname, '../../static/products', imageName);
+        if(!existsSync(path)){
+            throw new BadRequestException(`No product find with image ${imageName}`);
+        }
+        return path
+    }
+
 
 }
