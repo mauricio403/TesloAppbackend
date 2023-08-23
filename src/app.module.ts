11a35ec4b9242b755d6386e5c6d033ef0cfdda4d
Luis Mauricio Matango Mendoza <lmatango@ec.krugercorp.com>
2023-08-23 13:15:28 -0500

    add auth entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -32,7 +33,9 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 
     SeedModule,
 
-    FilesModule
+    FilesModule,
+
+    AuthModule
   ],
 })
 export class AppModule { }
---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,5 @@
+import { Entity } from "typeorm";
+
+
+@Entity('users')
+export class User { }
